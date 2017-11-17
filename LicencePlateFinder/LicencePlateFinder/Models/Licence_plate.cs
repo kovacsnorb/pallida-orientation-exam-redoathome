2535def9f8e3e51981f19da4f47080d317462197
kovacsnorb <kovacsnorb@gmail.com>
2017-11-17 17:18:52 +0100

    Packages done, basic MVD done, one model is done

---
+++ LicencePlateFinder/LicencePlateFinder/Models/Licence_plate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicencePlateFinder.Models
+{
+    public class Licence_plate
+    {
+        public long Id { get; set; }
+        public string Plate { get; set; }
+        public string Car_brand { get; set; }
+        public string Car_model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+}
